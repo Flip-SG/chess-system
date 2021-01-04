b23f31499a1cf7a0c4bc5d747dfe39a3c9fb1bca
Felipe Sales Gonçalves <felipesales@gmail.com>
2021-01-03 22:38:14 -0300

    dev: First class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Java World!!!");
+		Position pos = new Position(3, 6);
+		System.out.println(pos);
 
 	}
 
